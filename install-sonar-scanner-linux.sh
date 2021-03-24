12bf6d04b21b634ce7a04dbb39911d238fef97da
Jorge Santos <jsantos@tse.org.gt>
2021-03-24 16:16:45 +0000

    update install sonar scanner in linux

--- install-sonar-scanner-linux.sh
+++ install-sonar-scanner-linux.sh
@@ -3,22 +3,30 @@
 cd /tmp || exit
 echo "Downloading sonar-scanner....."
 if [ -d "/tmp/sonar-scanner-cli-4.2.0.1873-linux.zip" ];then
+    echo "delete current install zip"
     sudo rm /tmp/sonar-scanner-cli-4.2.0.1873-linux.zip
 fi
+
+if [ -d "/tmp/sonar-scanner-4.2.0.1873-linux" ];then
+    echo "delete current install folder"
+    sudo rm -r /tmp/sonar-scanner-4.2.0.1873-linux
+fi
+
 wget -q https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-linux.zip
 echo "Download completed."
 
 echo "Unziping downloaded file..."
-unzip sonar-scanner-cli-4.2.0.1873-linux.zip
+unzip /tmp/sonar-scanner-cli-4.2.0.1873-linux.zip
 echo "Unzip completed."
-rm ssonar-scanner-cli-4.2.0.1873-linux.zip
 
 echo "Installing to opt..."
-if [ -d "/var/opt/sonar-scanner-cli-4.2.0.1873-linux" ];then
-    sudo rm -rf /var/opt/sonar-scanner-cli-4.2.0.1873-linux
+if [ -d "/var/opt/sonar-scanner-4.2.0.1873-linux" ];then
+    echo "delete sonar scanner in opt"
+    sudo rm -rf /var/opt/sonar-scanner-4.2.0.1873-linux
 fi
-sudo mv sonar-scanner-cli-4.2.0.1873-linux /var/opt
+
+sudo mv /tmp/sonar-scanner-4.2.0.1873-linux /var/opt
 
 echo "Installation completed successfully."
-
-echo "You can use sonar-scanner!"
+echo "You can use sonar-scanner! in /var/opt/sonar-scanner-4.2.0.1873-linux/bin/sonar-scanner"
+sudo /var/opt/sonar-scanner-4.2.0.1873-linux/bin/sonar-scanner -v
